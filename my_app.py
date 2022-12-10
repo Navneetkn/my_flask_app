47a5ef57462d2d7f42d3f001d076644467843b7b
Navaneet Kangokar (Group) <navaneet.kangokar@global.ntt>
2022-12-10 14:07:16 +0530

    adding version details and .env file

--- my_app.py
+++ my_app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+version = os.environ.get("VERSION")
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello!! this is main page</h1>"
+    return "<p>Version = {}</p><h1>Hello!! this is main page</h1>".format(version)
 
 @app.route("/<name>")
 def user(name):
