6706e9b9fd280180c89fffeb0d9f495f195c462e
Navaneet Kangokar <kangokarnavaneet@gmail.com>
2022-12-11 23:16:39 +0530

    Testing merge branch

--- my_app.py
+++ my_app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 def home():
     return "<p>Version = {}</p><h1>Hello!! this is main page</h1>".format(version)
 
-# @app.route("/<name>")
-# def user(name):
-#     return f"<h1>hello {name}<h1>"
+@app.route("/<name>")
+def user(name):
+    return f"<h1>hello {name}<h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
